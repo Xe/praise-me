29a4124f213fb8b2347da5835d949971422107af
Xe Iaso <me@xeiaso.net>
2024-08-04 11:46:45 -0400

    meta tags
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,6 +8,28 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Praise me!</title>
         <link rel="stylesheet" href="/styles.css" />
+        <title>Praise my GitHub profile!</title>
+        <meta name="title" content="Praise my GitHub profile!" />
+        <meta
+          name="description"
+          content="Use your GitHub profile as a way to get personalized messages of praise and encouragement."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://praise-me.fly.dev/" />
+        <meta property="og:title" content="Praise my GitHub profile!" />
+        <meta
+          property="og:description"
+          content="Use your GitHub profile as a way to get personalized messages of praise and encouragement."
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://praise-me.fly.dev/" />
+        <meta property="twitter:title" content="Praise my GitHub profile!" />
+        <meta
+          property="twitter:description"
+          content="Use your GitHub profile as a way to get personalized messages of praise and encouragement."
+        />
       </head>
       <body class="bg-emerald-100 dark:bg-emerald-900 dark:text-gray-50">
         <div class="p-4 mx-auto max-w-screen-md">
