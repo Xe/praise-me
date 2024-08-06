9f796ed01978b98cceaf227e03e30bd998f02090
Xe Iaso <me@xeiaso.net>
2024-08-06 07:24:08 -0500

    remove some code
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,21 +4,8 @@ export default function Home() {
   return (
     <>
       <PraiseBox />
-      <script
-        async
-        src="https://media.ethicalads.io/media/client/ethicalads.min.js"
-      >
-      </script>
 
       <p class="py-4">Powered by Llama 3.1 70b and Fly.io GPUs.</p>
-
-      <div
-        className="bg-white dark:bg-black"
-        data-ea-publisher="christinewebsite"
-        data-ea-type="text"
-      >
-        An ad would go here if you didn't block them {":)"}
-      </div>
 
       {
         /*
